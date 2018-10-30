9a65a0f7bc8dcc14932b4606436f53b2adce5f7f
hmwverbeek <h.m.w.verbeek@tue.nl>
2018-10-30 08:55:36 +0000

    Added clickable ProM logo to Package Manager.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/ProM-Contexts/Trunk@40433 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/contexts/uitopia/packagemanager/PMSearchView.java
+++ src/org/processmining/contexts/uitopia/packagemanager/PMSearchView.java
@@ -3,9 +3,15 @@ package org.processmining.contexts.uitopia.packagemanager;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
+import org.deckfour.uitopia.ui.util.ImageLoader;
+import org.deckfour.uitopia.ui.util.LinkLabel;
 import org.deckfour.uitopia.ui.util.Tooltips;
 
 import com.fluxicon.slickerbox.components.RoundedPanel;
@@ -30,7 +36,7 @@ public class PMSearchView extends RoundedPanel {
 
 	private void setupUI() {
 		// Filter: Text search
-		filterSearch = new SlickerSearchField(50, 30, new Color(140, 140, 140), new Color(80, 80, 80),
+		filterSearch = new SlickerSearchField(50, 40, new Color(140, 140, 140), new Color(80, 80, 80),
 				new Color(40, 40, 40), new Color(20, 20, 20));
 		filterSearch.requestFocusInWindow();
 		filterSearch.setFont(filterSearch.getFont().deriveFont(Font.BOLD, 14.0f));
@@ -40,7 +46,14 @@ public class PMSearchView extends RoundedPanel {
 			}
 		});
 		filterSearch.setToolTipText(Tooltips.ACTIONFILTERSEARCHFIELD);
-		this.add(filterSearch);
+		this.add(filterSearch);		
+		
+		// Add clickable ProM icon.
+		Image appIcon = ImageLoader.load("prom_logo_101x40.png");
+		JLabel logoLabel = new LinkLabel(appIcon, "http://www.promtools.org");
+		logoLabel.setOpaque(false);
+		logoLabel.setBorder(BorderFactory.createEmptyBorder());
+		this.add(logoLabel, BorderLayout.WEST);
 	}
 
 	private void updateFilter() {
